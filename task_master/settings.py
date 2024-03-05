a3e3d7e10292798357993b8a7ae5a237a61b0290
asifalam515 <asibulalam515@gmail.com>
2024-03-05 22:12:09 +0600

    database updated

--- task_master/settings.py
+++ task_master/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-k#if&cx90@+9%cplrk(_n0vgq@rrxsn(giqt%h_(z6@8wgga$g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['task-master-django.onrender.com','https://*.127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://task-master-django.onrender.com','https://*.127.0.0.1']
+ALLOWED_HOSTS = ['task-master-django.onrender.com','https://*.127.0.0.1','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://task-master-django.onrender.com']
 
 
 # Application definition
@@ -94,11 +94,23 @@ WSGI_APPLICATION = 'task_master.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': env("DB_NAME"),
+#        'USER': env("DB_USER"),
+#        'PASSWORD': env("DB_PASSWORD"),
+#        'HOST': env("DB_HOST"),
+#        'PORT': env("DB_PORT"),
+#    }
+# }
+
+
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgres://task_master_csop_user:dUhi5NKiYv4VsKeRIduU6UzalRlh237l@dpg-cnjg89uct0pc73cauh80-a.oregon-postgres.render.com/task_master_csop'
+        default='postgres://task_master_6d7u_user:HihwaeDUH9ca2XHgIuYkkrvdg7J0LdbM@dpg-cnjjpu6v3ddc738d9l30-a.oregon-postgres.render.com/task_master_6d7u'
     )
 }
 
